7fa8702454e71ec636d9d78c241843c1eb5660ea
Vito <spadavecchio@gmail.com>
2017-05-20 14:51:31 -0700

    rewrite to include Gmail

--- controller.py
+++ controller.py
@@ -1,40 +1,58 @@
+import os
+import gmail
+from gmail import Gmail
 
 
-def parse_source(source):
-    """ determines media type from command """
-    commandstring = source.split()
-    command = commandstring[0]
-
-    return command
-
-
-def get_media_type(command, source ):
-    """ determines media type based on user input"""
-    if source =='audio':
-        #figure out audio interpretation
-    elif source == 'txt':
-        parse_source(source)
-
-
-
-    if command == 'play':
-        media_type = music
-    elif command == 'show':
-        media_type = movie
-
-    return media_type
+def check_email():
+    client = Gmail()
+    USERNAME = 'ifthisthenthatclone'
+    PASSWORD = 'ifthisthenthat'
+    client.login(USERNAME,PASSWORD)
 
 
 
 
+    if client.inbox().mail(unread=True):
 
+        unread = client.inbox().mail(unread=True)
+        unread[0].fetch()
+        print "The input string is {}".format(unread[0].body)
+        print ""
+        # FORMAT MUST BE (ARTIST, SONG NAME)
+
+        input_string = unread[0].body
+        input_split = input_string.split(',')
+        artist=input_split[0].lower()
+        song_name=input_split[1].lower()
+        print 'artist {}'.format(artist)
+
+
+        return artist,  song_name
+
+
+
+
+
+
+def play_media(artist, song_name):
+    """ Plays the media of the string passed to it. Supports the addition to an existing
+    playlist (default), or to play now (flag --now)"""
+
+
+    player = 'cvlc '
+    music_dir = '~/Music/'
+    cmd =  str(player + music_dir + artist + '/' + song_name + '.mp3')
+    print cmd
+
+    os.system(cmd)
 
 
 
 
 
 def main():
-
+    artist, song_name = check_email()
+    play_media(artist, song_name)
 
 
 
